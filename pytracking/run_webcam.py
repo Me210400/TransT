0fb40632a032767f886db171740fd31294afe285
Vahagn Melkonyan <vahmelk99@gmail.com>
2023-01-09 11:12:07 +0400

    TransT for ArmenU

--- pytracking/run_webcam.py
+++ pytracking/run_webcam.py
@@ -9,7 +9,7 @@ if env_path not in sys.path:
 from pytracking.evaluation import Tracker
 
 
-def run_webcam(tracker_name, tracker_param, debug=None, visdom_info=None):
+def run_webcam(debug=None, visdom_info=None):
     """Run the tracker on your webcam.
     args:
         tracker_name: Name of tracking method.
@@ -18,14 +18,14 @@ def run_webcam(tracker_name, tracker_param, debug=None, visdom_info=None):
         visdom_info: Dict optionally containing 'use_visdom', 'server' and 'port' for Visdom visualization.
     """
     visdom_info = {} if visdom_info is None else visdom_info
-    tracker = Tracker(tracker_name, tracker_param)
-    tracker.run_webcam(debug, visdom_info)
+    tracker = Tracker('transt', 'transt50')
+    return tracker.run_webcam(debug, visdom_info)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Run the tracker on your webcam.')
-    parser.add_argument('tracker_name', type=str, help='Name of tracking method.')
-    parser.add_argument('tracker_param', type=str, help='Name of parameter file.')
+    parser.add_argument('--tracker_name', default='transt', type=str, help='Name of tracking method.')
+    parser.add_argument('--tracker_param', default='transt50', type=str, help='Name of parameter file.')
     parser.add_argument('--debug', type=int, default=0, help='Debug level.')
     parser.add_argument('--use_visdom', type=bool, default=True, help='Flag to enable visdom')
     parser.add_argument('--visdom_server', type=str, default='127.0.0.1', help='Server for visdom')
@@ -34,7 +34,7 @@ def main():
     args = parser.parse_args()
 
     visdom_info = {'use_visdom': args.use_visdom, 'server': args.visdom_server, 'port': args.visdom_port}
-    run_webcam(args.tracker_name, args.tracker_param, args.debug, visdom_info)
+    run_webcam(args.debug, visdom_info)
 
 
 if __name__ == '__main__':
